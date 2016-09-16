65f164c5d46989ad1e11cc714d6aae8cfee1c769
Ruiwen <ruiwenli@usc.edu>
2016-09-15 22:40:33 -0700

    Add Shots

---
+++ unity/Space Defender/Assets/Script/Manager/Shots.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shots : MonoBehaviour {
+	GameObject targetObject;
+	Transform target;
+	float speed=5f;
+	public static string targetName;
+	public Victim victim;
+
+	public Shots(Victim vic){
+		victim = vic;
+		targetName = vic.gameObjectName;
+	}
+	// Use this for initialization
+	void Start () {
+		targetObject = GameObject.Find (targetName);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		target = target.transform;
+		//Debug.Log(target.position);
+		Vector3 dir = target.position - this.transform.position;
+		float framDist = speed * Time.deltaTime;
+		if (dir.magnitude <= framDist) {
+			SelfDestruct();
+			victim.dealDamage ();
+		} else {
+			transform.Translate(dir.normalized * framDist,Space.World);
+			this.transform.rotation = Quaternion.LookRotation (dir);
+		}
+	}
+	void SelfDestruct(){
+		Destroy (gameObject);
+	}
+}
