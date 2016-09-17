5d28922fb0eaf0408e78e02a36c23c1d540811a7
Anming Zhou <corvannia@gmail.com>
2016-09-16 22:40:27 -0700

    Add models for turrets
    1. Add three models with texture
    2. Turret Large - 425mm hybrid railgun
    Turret Medium - dual 150mm hybrid railgun
    Turret Small - 75mm hybrid railgun

---
+++ unity/Space Defender/Script/GUI/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+    
+    [System.Serializable]
+    public class Action
+    {
+        public Color color;
+        public Sprite sprite;
+        public string title;
+    }
+    public Action[] options;
+	// Use this for initialization
+	void OnMouseDown()
+    {
+        //spawn a menu
+        CircleSpwaner.ins.SpawnMenu(this);
+    }
+}
---
+++ unity/Space Defender/Script/Manager/Dispatcher.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dispatcher : MonoBehaviour {
+
+    private TimerManager tm = new TimerManager();
+    private Dictionary<string, Killer> killers = new Dictionary<string, Killer>();
+    private Dictionary<string, Victim> victims = new Dictionary<string, Victim>();
+    // Use this for initialization
+    void Start() {
+        print("Dispatcher start");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void RegisteKiller(string instanceID, Killer killer) {
+        if (this.killers.ContainsKey(instanceID)) {
+            return;
+        } else {
+            this.killers.Add(instanceID, killer);
+            return;
+        }
+    }
+
+    public void DeregisterKiller(string instanceID) {
+        if (this.killers.ContainsKey(instanceID)) {
+            this.killers.Remove(instanceID);
+        }
+        return;
+    }
+
+    public void RegisteVictim(string instanceID, Victim victim) {
+        if (this.victims.ContainsKey(instanceID)) {
+            return;
+        } else {
+            this.victims.Add(instanceID, victim);
+            return;
+        }
+    }
+
+    public void DeregisterVictim(string instanceID) {
+        if (this.victims.ContainsKey(instanceID)) {
+            this.victims.Remove(instanceID);
+        }
+        return;
+    }
+}
