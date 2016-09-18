d7a6274108859319f68d63a9f683833639755e7b
code4good0bad <blueredfff@126.com>
2016-09-17 17:24:44 -0700

    add a gfameEnd scene, Add waves into init, and modify some funcs in scoreBoard

--- unity/Space Defender/Assets/Script/Manager/ScoreBoard.cs
+++ unity/Space Defender/Assets/Script/Manager/ScoreBoard.cs
@@ -5,36 +5,47 @@ using UnityEngine.UI;
 public class ScoreBoard : MonoBehaviour {
     int live = 20;
     int fund = 100;
+    int wave = 10;
     public Text Funds;
     public Text Lives;
-    //初始游戏，生命值金钱
-    public void initGame(int lives, int funds) {
+    //initial game with life and funds and waves
+    public void initGame(int lives, int funds, int waves) {
         live = lives;
         fund = funds;
+        wave = waves;
     }
-    // 调用loseLife，控制生命损失 >0掉血，<0加血
+    // loseLife， >0 lose，<0 add 
     public void loseLife(int i) {
         live -= i;
         if (live <= 0) {
             gameOver();
         }
     }
-    //loseFund,控制金钱 >0掉钱，<0加钱 若钱为空或小于0，返回false
+    //loseFund, >0 lose，<0 add if money is not enough ，return false
     public bool loseFund(int i) {
         int temp = fund;
         fund -= i;
         if (fund <= 0) {
             fund = temp;
+            Debug.Log("no enough money");
             return false;
         }
         return true;
     }
+    //call this  every end of a wave
+    public void gameEnd(){
+        wave -= 1;
+        if (wave == 0){
+            SceneManager.LoadScene("GameEnd");
+        }
+    }
     //未完成状态
     public void gameOver() {
         Debug.Log("Game Over!");
+        SceneManager.LoadScene("GameEnd");
     }
     void Start() {
-        initGame(50, 1000);
+        initGame(50, 1000,30);
         Debug.Log(Funds.text);
     }
     //Update is called once per frame
