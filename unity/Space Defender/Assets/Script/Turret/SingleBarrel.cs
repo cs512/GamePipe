bb23e86e9658d4a400e34a114f7565bd3622edae
Anming Zhou <corvannia@gmail.com>
2016-09-18 16:59:11 -0700

    Create base class for turrets. Specify two classes for single-barrel and double-barrel turret. Modified some prefabs of turrets.

---
+++ unity/Space Defender/Assets/Script/Turret/SingleBarrel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+
+public class SingleBarrel : TurretBase {
+
+    public Transform shotSpawn;
+    public GameObject shot;
+
+    public override void SetUpAttributions() {
+        return;
+    }
+    override public void ShotSpawn() {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
