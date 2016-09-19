9aec68ebac5dff52b93a14c183589ff0ed4f9b57
Anming Zhou <corvannia@gmail.com>
2016-09-19 01:23:44 -0700

    Add touchscreen input code for testing. Rearrange and delete some files in folders for better accessibilty Change project platform to iOS system.

--- unity/Space Defender/Assets/Script/GUI/CircleMenu.cs
+++ unity/Space Defender/Assets/Script/GUI/CircleMenu.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CircleMenu : MonoBehaviour {
 
@@ -6,8 +8,14 @@ public class CircleMenu : MonoBehaviour {
     public CircleButton selected;
     public GameObject newObject;
 
-	// Use this for initialization
-	public void SpawnButtons (Interactable obj) {
+    public LayerMask touchInputMask;
+
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+    private RaycastHit hit;
+
+    // Use this for initialization
+    public void SpawnButtons (Interactable obj) {
         for (int i = 0;i < obj.options.Length; i++)
         {
             CircleButton newButton = Instantiate(buttonPrefab) as CircleButton;
@@ -25,33 +33,66 @@ public class CircleMenu : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (selected)
-            {
+        if (Input.touchCount > 0) {
+
+            touchesOld = new GameObject[touchList.Count];
+
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            foreach (Touch touch in Input.touches) {
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out hit, touchInputMask)) {
+
+                    GameObject recipient = hit.transform.gameObject;
+                    touchList.Add(recipient);
+
+                    if (touch.phase == TouchPhase.Ended) {
+                        if (selected) {
+                            Debug.Log(selected.title + " was selected");
+                            transform.position = new Vector3(380f, 0f, 120f);
+                            transform.localRotation = new Quaternion(0f, 90f, 0f, 0f);
+                            if (selected.title == "trtLrg") {
+                                Object prefab = Resources.Load("Prefabs/G425", typeof(GameObject)) as GameObject;
+                                newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
+                            } else if (selected.title == "trtMd") {
+                                Object prefab = Resources.Load("Prefabs/G150dual", typeof(GameObject)) as GameObject;
+                                newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
+                            } else if (selected.title == "trtSml") {
+                                Object prefab = Resources.Load("Prefabs/G75", typeof(GameObject)) as GameObject;
+                                newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
+                            }
+
+                        }
+                        Destroy(gameObject);
+                    }
+                }
+            }
+            foreach (GameObject g in touchesOld) {
+                if (!touchList.Contains(g)) {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonUp(0)){
+            if (selected) {
                 Debug.Log(selected.title + " was selected");
-                transform.position = new Vector3 (380f, 0f, 120f);
+                transform.position = new Vector3(380f, 0f, 120f);
                 transform.localRotation = new Quaternion(0f, 90f, 0f, 0f);
-                if (selected.title == "trtLrg")
-                {
+                if (selected.title == "trtLrg") {
                     Object prefab = Resources.Load("Prefabs/G425", typeof(GameObject)) as GameObject;
                     newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
-                }
-                else if (selected.title == "trtMd")
-                {
+                } else if (selected.title == "trtMd") {
                     Object prefab = Resources.Load("Prefabs/G150dual", typeof(GameObject)) as GameObject;
                     newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
-                }
-                else if (selected.title == "trtSml")
-                {
+                } else if (selected.title == "trtSml") {
                     Object prefab = Resources.Load("Prefabs/G75", typeof(GameObject)) as GameObject;
                     newObject = Instantiate(prefab, transform.position, transform.localRotation) as GameObject;
                 }
-                
             }
             Destroy(gameObject);
-
         }
-
     }
+#endif
 }
--- unity/Space Defender/Assets/Script/GUI/Interactable.cs
+++ unity/Space Defender/Assets/Script/GUI/Interactable.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Interactable : MonoBehaviour {
-    
+    public LayerMask touchInputMask;
+
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+    private RaycastHit hit;
+
     [System.Serializable]
     public class Action
     {
@@ -11,10 +17,42 @@ public class Interactable : MonoBehaviour {
         public string title;
     }
     public Action[] options;
-	// Use this for initialization
-	void OnMouseDown()
-    {
-        //spawn a menu
-        CircleSpwaner.ins.SpawnMenu(this);
+    // Use this for initialization
+
+#if UNITY_EDITOR
+    void OnMouseDown() {
+        if (Input.GetMouseButtonDown(0)) {
+            CircleSpwaner.ins.SpawnMenu(this);
+        }
     }
+#else
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+
+            touchesOld = new GameObject[touchList.Count];
+
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            foreach (Touch touch in Input.touches) {
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out hit, touchInputMask)) {
+
+                    GameObject recipient = hit.transform.gameObject;
+                    touchList.Add(recipient);
+
+                    if (touch.phase == TouchPhase.Began) {
+                        CircleSpwaner.ins.SpawnMenu(this);
+                    }
+                }
+            }
+            foreach (GameObject g in touchesOld) {
+                if (!touchList.Contains(g)) {
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+#endif
 }
