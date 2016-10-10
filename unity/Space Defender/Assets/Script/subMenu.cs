a2204f0b18ab56415c7e0c6b47f18c5caa4951ae
Anming Zhou <corvannia@gmail.com>
2016-10-09 23:10:59 -0700

    Remove UI_Test_1.unity

---
+++ unity/Space Defender/Assets/Script/subMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class subMenu : MonoBehaviour {
+
+    private GameObject newObject;
+    public GameObject panel;
+
+    public void createSubMenu() {
+        Object prefab = Resources.Load("Prefabs/levels", typeof(GameObject)) as GameObject;
+        newObject = Instantiate(prefab, panel.transform.position, panel.transform.rotation) as GameObject;
+        Destroy(panel);
+    }
+}
