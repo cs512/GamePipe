e68904fef4624e6b5ed486c0e36c9e49aa358979
J. Autsin Wang <junzhuow@usc.edu>
2016-10-09 23:05:40 -0700

    level manager constructor done

--- unity/Space Defender/Assets/Script/Manager/LevelManager/LevelManager.cs
+++ unity/Space Defender/Assets/Script/Manager/LevelManager/LevelManager.cs
@@ -18,9 +18,23 @@ class LevelManager : MonoBehaviour {
             Level level = new Level();
             level.name = levelList["levels"][i]["name"];
             var levelConfig = JSON.Parse(
-                (Resources.Load("GameData/"+ levelList["levels"][i]["fileName"])as TextAsset)
-                .text);
-            
+                (Resources.Load("GameData/"+ levelList["levels"][i]["fileName"])
+                as TextAsset).text);
+            for(int j = 0; j < levelConfig["waveCount"].AsInt; ++j) {
+                var waveJSON = levelConfig["waves"][j];
+                Level.Wave wave = new Level.Wave();
+                wave.waveDuring = waveJSON["waveDuring"].AsInt;
+                for(int k = 0; k < waveJSON["spawnPointCount"].AsInt; ++k) {
+                    var spJSON = waveJSON["spawnPoints"][k];
+                    Level.Wave.SpawnPoint sp = new Level.Wave.SpawnPoint();
+                    sp.interval = spJSON["interval"].AsInt;
+                    sp.number = spJSON["number"].AsInt;
+                    sp.prefab = spJSON["prefab"];
+                    sp.position = new Vector3(spJSON["position"][0].AsFloat, spJSON["position"][1].AsFloat, spJSON["position"][2].AsFloat);
+                    wave.spawnPoints.Add(sp);
+                }
+                level.waves.Add(wave);
+            }
         }
     }
 
