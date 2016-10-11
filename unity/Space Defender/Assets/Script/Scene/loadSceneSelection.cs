a6f8a1abd11c065cead5bb125935dd667c62b358
J. Autsin Wang <junzhuow@usc.edu>
2016-10-10 17:04:03 -0700

    finish level manager

--- unity/Space Defender/Assets/Script/Scene/loadSceneSelection.cs
+++ unity/Space Defender/Assets/Script/Scene/loadSceneSelection.cs
@@ -3,19 +3,21 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class loadSceneSelection : MonoBehaviour {
-    public void loadScene()
-    {
-        if (gameObject.name == "mainMenu")
-        {
+    public void loadScene() {
+        if (gameObject.name == "mainMenu") {
             SceneManager.LoadScene("opeing");
-        }
-        else if (gameObject.name == "tutorial")
-        {
+        } else if (gameObject.name == "tutorial") {
             SceneManager.LoadScene("UI_Test");
-        }
-        else if (gameObject.name == "Story")
-        {
+        } else if (gameObject.name == "Story") {
             SceneManager.LoadScene("levelSelection");
+        } else if (gameObject.name == "level1") {
+            Toolbox.Instance.GetComponent<LevelManager>().JumpLevel(1);
+        } else if (gameObject.name == "Level 2") {
+            Toolbox.Instance.GetComponent<LevelManager>().JumpLevel(2);
+        } else if (gameObject.name == "Level 3") {
+            Toolbox.Instance.GetComponent<LevelManager>().JumpLevel(3);
+        } else if (gameObject.name == "Level 4") {
+            Toolbox.Instance.GetComponent<LevelManager>().JumpLevel(4);
         }
 
     }
