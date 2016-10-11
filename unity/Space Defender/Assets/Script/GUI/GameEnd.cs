19be205b355f88249e3a4d9734ac31d756d00803
code4good0bad <blueredfff@126.com>
2016-10-10 22:46:27 -0700

    add gameEnd

---
+++ unity/Space Defender/Assets/Script/GUI/GameEnd.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour {
+    Scene activeScene = SceneManager.GetActiveScene();
+    public string path = "";
+    public void Retry() {// that is a reloading of this scene
+        if (activeScene.path != "")
+        {
+            path += activeScene.path;
+            SceneManager.LoadScene(path);
+        }
+        else
+            Debug.Log("Scene Wrong!");
+    }
+    public void Next() {//To the next level.
+        if (path != "")
+        {
+            SceneManager.LoadScene(path);
+        }
+        else
+            Debug.Log("Cant get next Level!");
+    }
+    public void Back() {//go back to the Level Selection
+        SceneManager.LoadScene("levelSelection");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
