b0ba03aa1e141b67d7ee50fdbcbed970e0f470e8
Radiomad <tim.yuduoguo@gmail.com>
2016-10-11 23:21:13 -0700

    turret slider in half way

--- unity/Space Defender/Assets/Script/Turret/TurretBase.cs
+++ unity/Space Defender/Assets/Script/Turret/TurretBase.cs
@@ -26,7 +26,7 @@ public abstract class TurretBase : MonoBehaviour, Killer,Victim {
     
     public GameObject explosion;
 
-	//public Slider healthSlider;
+	public Slider healthSlider;
 
 	private float maxHealth;
 
@@ -133,6 +133,7 @@ public abstract class TurretBase : MonoBehaviour, Killer,Victim {
         health -= damage;
         if (health <= 0f) {
             this.DestorySelf();
+			Destroy(healthSlider);
         }
 		SetHealthUI();
     }
@@ -154,6 +155,6 @@ public abstract class TurretBase : MonoBehaviour, Killer,Victim {
     }
 
 	void SetHealthUI() {
-		//healthSlider.value = health / maxHealth * 100;
+		healthSlider.value = health / maxHealth * 100;
 	}
 }
