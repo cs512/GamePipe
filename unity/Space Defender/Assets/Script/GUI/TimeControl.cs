7645b5ea6dfabc0a6b79068d9729a7bd24ba29da
Anming Zhou <corvannia@gmail.com>
2016-10-12 02:39:27 -0700

    Add time control.

---
+++ unity/Space Defender/Assets/Script/GUI/TimeControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeControl : MonoBehaviour {
+
+    public Text content;
+
+	public void pause()
+    {
+        if (content.text == "pause")
+        {
+            Time.timeScale = 0;
+            content.text = "start";
+        }
+        else if (content.text == "start")
+        {
+            Time.timeScale = 1;
+            content.text = "pause";
+        }
+    }
+    public void speedUp()
+    {
+        Time.timeScale += 2;
+    }
+    public void slowDown()
+    {
+        Time.timeScale -= 2;
+    }
+}
