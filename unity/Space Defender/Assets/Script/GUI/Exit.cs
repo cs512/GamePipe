36a0b0142420b1d3548d19a11704c8dc973d3d84
Anming Zhou <corvannia@gmail.com>
2016-10-27 00:38:22 -0700

    Rearranged and cleaned the work directory.

---
+++ unity/Space Defender/Assets/Script/GUI/Exit.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Exit : MonoBehaviour {
+    public void Back() {//go back to the Level Selection
+        SceneManager.LoadScene("levelSelection");
+    }
+}
