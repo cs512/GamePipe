9bd060acdd913c2b6ee063fcb7a19caaf27e0f40
Radiomad <tim.yuduoguo@gmail.com>
2016-10-31 17:25:58 -0700

    add shoot function for laser turret

---
+++ unity/Space Defender/Assets/Script/Movement/Laser.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	public float damage = 2.0f;
+	public LineRenderer laser;
+
+	private Vector3 start;
+	private Transform target;
+	private float remainTime = 0.1f;
+
+	void Update() {
+		KeepDirection();
+		CountTime();
+	}
+
+	public void SetUp(Vector3 start, Transform target) {
+		laser.SetPosition(0, start);
+		this.target = target;
+	}
+
+	void KeepDirection() {
+		Vector3 des = new Vector3(target.position.x, target.position.y, target.position.z);
+		laser.SetPosition(1, des);
+	}
+
+	void CountTime() {
+		if(remainTime <= 0) {
+			Destroy(gameObject);
+		} else {
+			remainTime -= Time.deltaTime;
+		}
+	}
+
+
+}
