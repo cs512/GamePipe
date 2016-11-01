147d9e75f423a2fdc8919b47ae92c333267bc1bc
Anming Zhou <corvannia@gmail.com>
2016-11-01 16:39:58 -0700

    1. Add indicator shows where enemys will spawn.

---
+++ unity/Space Defender/Assets/Script/GUI/SpawnerTargetController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerTargetController : MonoBehaviour {
+    void Update() {
+        transform.Rotate(0, 0, 20f * Time.deltaTime);
+
+        transform.localScale = new Vector3(Mathf.PingPong(Time.time, 0.5f) + 1f, Mathf.PingPong(Time.time, 0.5f) + 1f, transform.localScale.z);
+    }
+}
