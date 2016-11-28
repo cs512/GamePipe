2c5f7d09883faae2023411830201cddd45b23529
冯光照 <blueredfff@126.com>
2016-11-28 00:59:50 -0800

    push

--- unity/Space Defender/Assets/Script/Scene/loadSceneSelection.cs
+++ unity/Space Defender/Assets/Script/Scene/loadSceneSelection.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class loadSceneSelection : MonoBehaviour {
     public void loadScene() {
@@ -11,22 +12,37 @@ public class loadSceneSelection : MonoBehaviour {
         } else if (gameObject.name == "Story") {
             Toolbox.Instance.GetComponent<LevelManager>().SetMode(1);
             GameObject.Find("Main Camera").GetComponent<GameInitializator>().PerformSenceTransit();
-        } else if(gameObject.name == "Ski"){
+        } else if(gameObject.name == "Ski") {
+            //GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().SetMode(0);
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevel(1);
         } else if (gameObject.name == "level1Ski") {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(1);
         } else if (gameObject.name == "level2Ski") {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(2);
-        } else if (gameObject.name == "level3Ski") {
+        } else if (gameObject.name == "level3Ski")
+        {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(3);
-        } else if (gameObject.name == "level4Ski") {
+        } else if (gameObject.name == "level4Ski")
+        {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(4);
-        } else if (gameObject.name == "level5Ski") {
+        } else if (gameObject.name == "level5Ski")
+        {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(5);
-        } else if (gameObject.name == "level6Ski") {
+        } else if (gameObject.name == "level6Ski")
+        {
+            GameObject.Find("LoadingImage").GetComponent<Image>().enabled = true;
             Toolbox.Instance.GetComponent<LevelManager>().JumpLevelSki(6);
         }
 
+    }
+    void Start()
+    {
+        GameObject.Find("LoadingImage").GetComponent<Image>().enabled = false;
     }
 }
