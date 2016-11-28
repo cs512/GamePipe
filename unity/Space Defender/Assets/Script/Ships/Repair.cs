675b0e697bb3bf64091a759da18c08535b1d96ae
Radiomad <tim.yuduoguo@gmail.com>
2016-11-27 20:06:09 -0800

    add new ship prefabs

---
+++ unity/Space Defender/Assets/Script/Ships/Repair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Pathfinding;
+
+public class Repair : Enemy {
+	public void Start()
+    {
+        oldSpeed = speed; // slow
+        this.SetUpDefaultAttributions();
+        Dispatcher dispatcher = GameObject.Find("Dispatcher").GetComponent<Dispatcher>();
+        dispatcher.enemyRegisteVictim(this);
+        dispatcher.enemyRegisteKiller(this);
+
+        float x = target.position.x;
+        float z = target.position.z;
+        sourceTarget = new Vector3(x, this.transform.localPosition.y, z);
+
+        maxHealth = health; // slider
+        InvokeRepeating("Forwards", 0f, 0.05f);
+        InvokeRepeating("RecoverSpeed", 0f, 2f);
+        SetAbility();
+    }
+
+	public new void SetAbility()
+	{       
+		InvokeRepeating("RecoverHealth", 0f, 1f);
+	}
+
+
+
+	public override void SetUpDefaultAttributions() {
+
+	}
+}
