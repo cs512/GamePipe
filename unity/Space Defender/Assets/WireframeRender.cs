651aa2c1f2a45db24ac95b5152abeccb6ef6f747
冯光照 <blueredfff@126.com>
2016-11-27 21:41:34 -0800

    change blend

---
+++ unity/Space Defender/Assets/WireframeRender.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireframeRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GL.wireframe = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GL.wireframe = true;
+    }
+
+    void OnPreRender()
+    {
+        GL.wireframe = true;
+    }
+
+    void OnPostRender()
+    {
+        GL.wireframe = false;
+    }
+}
